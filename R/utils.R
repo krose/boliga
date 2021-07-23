65a802abe9352c4bb067d874b4f8cc695542f764
mozin <mohsinvindhani@gmail.com>
2021-07-23 19:26:46 +0530

    init commit

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,7 @@
 
 bol_extract_table <- function(boliga_content){
   
+  browser()
   # parse the text to html
   # find the #searchresult nodes
   # get the table rows and content
@@ -27,59 +28,55 @@ bol_extract_table <- function(boliga_content){
 
   pris <- 
     bol_table %>%
-    purrr::map(~.x %>% .[2] %>% rvest::html_node("h5") %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
+    purrr::map(~.x %>% .[2] %>% rvest::html_nodes("span") %>% .[2] %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
     do.call(c, .) %>%
     as.integer()
   
-  dato_type_tibble <- 
+  salgsdato <- 
     bol_table %>%
-    purrr::map(~.x %>% .[3] %>% rvest::html_node("h5")) %>%
-    purrr::map(~as.character(.x)) %>% 
-    purrr::map(~stringr::str_replace_all(.x, "<br>", ",")) %>% 
+    purrr::map(~.x %>% .[3] %>% rvest::html_nodes("span") %>% .[2] %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
     do.call(c, .) %>% 
-    paste(., collapse = "\n") %>% 
-    xml2::read_html() %>% 
-    rvest::html_nodes("h5") %>% 
-    rvest::html_text() %>% 
-    stringr::str_split(., ",", simplify = TRUE) %>% 
-    magrittr::set_colnames(c("dato", "salgstype")) %>% 
-    tibble::as_tibble() %>% 
-    dplyr::mutate(dato = as.Date(dato, format = "%d-%m-%Y"))
+    as.Date(dato, format = "%d-%m-%Y")
   
+  boligtype <- 
+    bol_table %>%
+    purrr::map(~.x %>% .[4] %>% rvest::html_nodes("span") %>% .[4] %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
+    do.call(c, .)
+    
   pris_kvm <- 
     bol_table %>%
-    purrr::map(~.x %>% .[4] %>% rvest::html_node("h5") %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
+    purrr::map(~.x %>% .[5] %>% rvest::html_nodes("span") %>% .[2] %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
     do.call(c, .) %>% 
     as.integer()
   
   vaerelser <- 
     bol_table %>%
-    purrr::map(~.x %>% .[5] %>% rvest::html_node("h5") %>% rvest::html_text()) %>%
+    purrr::map(~.x %>% .[6] %>% rvest::html_nodes("span") %>% .[2] %>% rvest::html_text()) %>%
     do.call(c, .) %>%
     as.integer()
   
-  type <- 
-    bol_table %>%
-    purrr::map(~.x %>% .[6] %>% rvest::html_node("h5") %>% rvest::html_text()) %>%
-    do.call(c, .)
-
-  kvm <- 
-    bol_table %>%
-    purrr::map(~.x %>% .[7] %>% rvest::html_node("h5") %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
-    do.call(c, .) %>%
-    as.integer()
-  
-  bygget_aar <- 
-    bol_table %>%
-    purrr::map(~.x %>% .[8] %>% rvest::html_node("h5") %>% rvest::html_text()) %>%
-    do.call(c, .)
-  
-  udbudsrabat <- 
-    bol_table %>%
-    purrr::map(~.x %>% .[9] %>% rvest::html_node("h5") %>% rvest::html_text()) %>%
-    do.call(c, .) %>%
-    stringr::str_replace(., " %", "") %>%
-    as.double()
+  # type <- 
+  #   bol_table %>%
+  #   purrr::map(~.x %>% .[6] %>% rvest::html_node("h5") %>% rvest::html_text()) %>%
+  #   do.call(c, .)
+  # 
+  # kvm <- 
+  #   bol_table %>%
+  #   purrr::map(~.x %>% .[7] %>% rvest::html_node("h5") %>% rvest::html_text() %>% stringr::str_replace_all(., "\\.", "")) %>%
+  #   do.call(c, .) %>%
+  #   as.integer()
+  # 
+  # bygget_aar <- 
+  #   bol_table %>%
+  #   purrr::map(~.x %>% .[8] %>% rvest::html_node("h5") %>% rvest::html_text()) %>%
+  #   do.call(c, .)
+  # 
+  # udbudsrabat <- 
+  #   bol_table %>%
+  #   purrr::map(~.x %>% .[9] %>% rvest::html_node("h5") %>% rvest::html_text()) %>%
+  #   do.call(c, .) %>%
+  #   stringr::str_replace(., " %", "") %>%
+  #   as.double()
   
   bolig_link <- 
     bol_table %>% 
